3f09b2a6302b11399b21674693c9314a16324e9e
Åke Engelbrektson <eson57@users.noreply.github.com>
2021-02-08 12:47:36 +0100

    Create harbour-rwx-calc-sv.ts
    Add Swedish translation please.

---
+++ translations/harbour-rwx-calc-sv.ts
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="22"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="25"/>
+        <source>Info</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="45"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="56"/>
+        <source>Author</source>
+        <translation>Utvecklare</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="307"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="311"/>
+        <source>Umask explained</source>
+        <translation>Umask förklarad</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="315"/>
+        <source>Help</source>
+        <translation>Hjälp</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="332"/>
+        <source>rwx-calc</source>
+        <translation>rwx-calc</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="337"/>
+        <source>Permissions calculator</source>
+        <translation>Behörighetskalkylator</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="366"/>
+        <location filename="../qml/pages/MainPage.qml" line="535"/>
+        <source>Special</source>
+        <translation>Special</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="597"/>
+        <source>Absolute (octal) and symbolic</source>
+        <translation>Absolut (oktal) och symbolisk</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="609"/>
+        <source>
+Absolute (octal) &amp; symbolic</source>
+        <translation>
+Absolut (oktal) &amp; symbolisk</translation>
+    </message>
+</context>
+<context>
+    <name>Octal</name>
+    <message>
+        <location filename="../qml/pages/Octal.qml" line="37"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Octal.qml" line="41"/>
+        <source>Umask explained</source>
+        <translation>Umask förklarad</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Octal.qml" line="45"/>
+        <source>Help</source>
+        <translation>Hjälp</translation>
+    </message>
+</context>
+</TS>
